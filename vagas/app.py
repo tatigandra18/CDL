de1df8fcad110932dde9157939261cf96aff4002
tatigandra18 <153555507+tatigandra18@users.noreply.github.com>
2024-06-28 08:52:20 -0300

    Add files via upload

--- vagas/app.py
+++ vagas/app.py
@@ -1,8 +1,7 @@
 
 import streamlit as st
+import pandas as pd
 import plotly.express as px
-import pandas as pd
-import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 
