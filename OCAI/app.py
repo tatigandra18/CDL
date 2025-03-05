1e27cad0dca84f9d538515f6badaf7b14e59c8c5
tatigandra18 <153555507+tatigandra18@users.noreply.github.com>
2025-03-05 15:50:23 -0300

    Update app.py

--- OCAI/app.py
+++ OCAI/app.py
@@ -65,7 +65,6 @@ def radar_chart(z_scores_current, z_scores_desired, labels, title, key=None):
         margin=dict(l=100, r=100, t=100, b=100)  # Ajuste das margens para evitar corte dos rótulos
     )
     
-    # Passar a chave única no st.plotly_chart
     st.plotly_chart(fig, use_container_width=True, key=key)
 
 # Título do app
@@ -110,7 +109,7 @@ with tab1:
 
     # Exibir gráfico geral para empresa
     st.subheader('Diferença em Z-Scores - Média Geral da Empresa')
-
+    
     empresa_filtro = data[data['Empresas'] == empresas]
     if not empresa_filtro.empty:
         z_scores_current_avg = z_scores_current.loc[empresa_filtro.index].mean().values.tolist()
@@ -119,35 +118,32 @@ with tab1:
         radar_chart(z_scores_current_avg, z_scores_desired_avg, labels, f"Média Geral - {empresas} (Z-Scores)", key="radar_empresa")
 
 # Aba 2: Comparação entre Empresas
-  with tab2:
-      st.subheader('Comparação entre Empresas')
-  
-      empresas_unicas = sorted(data['Empresas'].unique())
-  
-      for i, empresa in enumerate(empresas_unicas):
-          empresa_filtro = data[data['Empresas'] == empresa]
-          
-          if not empresa_filtro.empty:
-              z_scores_current_avg = z_scores_current.loc[empresa_filtro.index].mean().values.tolist()
-              z_scores_desired_avg = z_scores_desired.loc[empresa_filtro.index].mean().values.tolist()
-              
-              st.markdown(f"### {empresa}")
-              radar_chart(z_scores_current_avg, z_scores_desired_avg, labels, f"Média Geral - {empresa} (Z-Scores Globais)", key=f"radar_chart_{i}")
+with tab2:
+    st.subheader('Comparação entre Empresas')
+
+    empresas_unicas = sorted(data['Empresas'].unique())
+
+    for i, empresa in enumerate(empresas_unicas):
+        empresa_filtro = data[data['Empresas'] == empresa]
+        
+        if not empresa_filtro.empty:
+            z_scores_current_avg = z_scores_current.loc[empresa_filtro.index].mean().values.tolist()
+            z_scores_desired_avg = z_scores_desired.loc[empresa_filtro.index].mean().values.tolist()
+            
+            st.markdown(f"### {empresa}")
+            radar_chart(z_scores_current_avg, z_scores_desired_avg, labels, f"Média Geral - {empresa} (Z-Scores Globais)", key=f"radar_chart_{i}")
 
 # Aba 3: Maiores Índices
-    with tab3:
-        st.subheader('Top 10 Empresas com Maiores Índices')
-    
-        elements = ['Clã_atual', 'Adhocracia_atual', 'Mercado_atual', 'Hierarquia_atual']
-        for element in elements:
-            top_10 = data.groupby('Empresas')[element].mean().nlargest(10)
-            
-            fig = px.bar(top_10, x=top_10.index, y=top_10.values, labels={'y': 'Média', 'x': 'Empresa'},
-                         title=f'Top 10 Empresas com Maiores Índices de {element.split("_")[0]}')
-    
-            fig.update_layout(
-                width=800,  # Ajuste do tamanho do gráfico de barras
-                height=500
-            )
-            
-            st.plotly_chart(fig, use_container_width=True)
+with tab3:
+    st.subheader('Top 10 Empresas com Maiores Índices')
+
+    elements = ['Clã_atual', 'Adhocracia_atual', 'Mercado_atual', 'Hierarquia_atual']
+    for element in elements:
+        top_10 = data.groupby('Empresas')[element].mean().nlargest(10)
+        
+        fig = px.bar(top_10, x=top_10.index, y=top_10.values, labels={'y': 'Média', 'x': 'Empresa'},
+                     title=f'Top 10 Empresas com Maiores Índices de {element.split("_")[0]}')
+
+        fig.update_layout(width=800, height=500)
+        
+        st.plotly_chart(fig, use_container_width=True)
